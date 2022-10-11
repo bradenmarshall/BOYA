584f9325fe686698dfae4d3df486d25574cb6382
Braden Marshall <bmars385@d127.org>
2022-10-11 08:46:58 -0500

    Initial Commit

---
+++ BOYA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BOYA
+//
+//  Created by Braden N Marshall (Student) on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
